dfee01a47445df010621e5869192bf8d33146211
Saurabh <saurabh.bhalerao0904@gmail.com>
2024-12-10 22:30:08 +0530

    Updated

---
+++ src/test/java/com/testingacademy/example/testng/ddt/DDT.java
@@ -0,0 +1,12 @@
+package com.testingacademy.example.testng.ddt;
+
+public class DDT {
+
+    //Test data -> From the excel file - DataProvider
+    //Read the Excel file
+    //Apache POI - 60-70% - little difficult to understand first time
+    //Fillo library - 30% - Super Easy
+
+    //Testcase login where we can inject the username and password
+
+}
